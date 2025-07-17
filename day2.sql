3c73efdaf377b43377458f2166a302501c81bc38
ZaidShaikh <szaid5146@gmail.com>
2025-07-17 20:59:45 +0530

    SQL DAY 2

---
+++ day2.sql
@@ -0,0 +1,69 @@
+-- CREATE DATABASE IF NOT EXISTS startersql;
+-- USE startersql;
+-- CREATE TABLE users(
+-- id INT AUTO_INCREMENT PRIMARY KEY,
+-- name VARCHAR(100) NOT NULL,
+-- email VARCHAR(100) UNIQUE NOT NULL,
+-- gender ENUM('Male','Female','Other'),
+-- date_of_birth DATE,
+-- salary DECIMAL(10,2),
+-- created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- ADDING THE DATA NOW
+-- INSERT INTO users (name, email, gender, date_of_birth, salary) VALUES
+-- ('Aarav', 'aarav@example.com', 'Male', '1995-05-14', 65000.00),
+-- ('Ananya', 'ananya@example.com', 'Female', '1990-11-23', 72000.00),
+-- ('Raj', 'raj@example.com', 'Male', '1988-02-17', 58000.00),
+-- ('Sneha', 'sneha@example.com', 'Female', '2000-08-09', 50000.00),
+-- ('Farhan', 'farhan@example.com', 'Male', '1993-12-30', 61000.00),
+-- ('Priyanka', 'priyanka@example.com', 'Female', '1985-07-12', 84000.00),
+-- ('Aisha', 'aisha@example.com', 'Female', '1997-03-25', 56000.00),
+-- ('Aditya', 'aditya@example.com', 'Male', '1992-06-17', 69000.00),
+-- ('Meera', 'meera@example.com', 'Female', '1989-09-05', 77000.00),
+-- ('Ishaan', 'ishaan@example.com', 'Male', '2001-10-02', 45000.00),
+-- ('Tanvi', 'tanvi@example.com', 'Female', '1994-04-18', 62000.00),
+-- ('Rohan', 'rohan@example.com', 'Male', '1986-12-01', 75000.00),
+-- ('Zoya', 'zoya@example.com', 'Female', '1998-01-15', 54000.00),
+-- ('Karan', 'karan@example.com', 'Male', '1990-08-22', 68000.00),
+-- ('Nikita', 'nikita@example.com', 'Female', '1987-03-10', 71000.00),
+-- ('Manav', 'manav@example.com', 'Male', '1996-11-29', 61000.00),
+-- ('Divya', 'divya@example.com', 'Female', '1991-02-28', 57000.00),
+-- ('Harshit', 'harshit@example.com', 'Male', '1993-09-09', 65000.00),
+-- ('Ritika', 'ritika@example.com', 'Female', '1999-05-05', 52000.00),
+-- ('Imran', 'imran@example.com', 'Male', '1995-07-30', 63000.00),
+-- ('Juhi', 'juhi@example.com', 'Female', '1992-10-14', 59000.00),
+-- ('Tushar', 'tushar@example.com', 'Male', '1990-01-08', 73000.00),
+-- ('Lata', 'lata@example.com', 'Female', '1984-11-11', 78000.00),
+-- ('Yash', 'yash@example.com', 'Male', '1997-06-06', 64000.00),
+-- ('Fatima', 'fatima@example.com', 'Female', '1993-03-03', 55000.00);
+
+--                   QUERYING THE DATA
+-- SELECT * FROM users WHERE date_of_birth < '1998-05-14';
+-- SELECT * FROM users WHERE date_of_birth IS NOT NULL; 
+-- SELECT * FROM users WHERE date_of_birth BETWEEN '1990-09-09' AND '2000-09-09';
+-- SELECT * FROM users WHERE gender IN ('Male','Female');
+-- SELECT * FROM users WHERE gender = 'Female' AND salary < 70000;
+-- SELECT * FROM users WHERE gender = 'Male' OR salary >70000 ORDER BY salary DESC LIMIT 3;
+-- SELECT * FROM users;
+
+--                    UPDATING THE DATA
+-- UPDATE users SET name = 'Zaid' WHERE id =1;
+-- UPDATE users SET email = 'szaid5146@gmail.com' WHERE id =1;
+-- UPDATE users SET date_of_birth = '2004-06-14' WHERE id = 1;
+-- UPDATE users SET name = 'Amita', email = 'Amita07@gmail.com', date_of_birth = '2004-01-12' WHERE id =2;
+-- UPDATE users SET salary = 70000 WHERE id =5;
+-- UPDATE users SET name = 'Aisha Khan' WHERE email = 'aisha@example.com';
+-- UPDATE users SET salary = salary + 10000 WHERE salary < 60000;
+-- DELETE FROM users WHERE id =3;
+--                    CHECK CONSTRAINTSS
+-- ALTER TABLE users ADD CONSTRAINT chk_dob CHECK (date_of_birth > '1920-04-29');
+--                   SQL FUNCTIONS
+-- select COUNT(*) FROM users;
+-- SELECT COUNT(*) FROM users WHERE gender = 'Male';
+-- SELECT MIN(salary) AS min_salary, MAX(salary) AS max_salary FROM users;
+-- SELECT SUM(salary) AS total_amount, COUNT(*) AS total_population FROM USERS;
+-- SELECT SUM(salary) AS total_sum, AVG(salary) AS avg_salary FROM users;
+-- SELECT gender,SUM(salary) AS total_salary, AVG(salary) AS avg_salary FROM users GROUP BY gender;
+-- SELECT gender, LENGTH(name) FROM users;
+SELECT YEAR(NOW()) AS year_of_birth, SUM(salary) AS total_salary, NOW() AS current_timee, YEAR(NOW()) AS curr_year FROM USERS WHERE id <=2;
